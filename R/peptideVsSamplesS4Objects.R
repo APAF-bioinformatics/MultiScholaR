9bf594af48ec21d324803ec67cb4543c40928722
Ignatius Pang <2321366+IgnatiusPang@users.noreply.github.com>
2025-10-15 22:39:36 +1100

    Updates for BookChapter

--- R/peptideVsSamplesS4Objects.R
+++ R/peptideVsSamplesS4Objects.R
@@ -1926,25 +1926,75 @@ setMethod(f="plotPca"
           })
 
 #'@export
+
 setMethod(f="plotDensity"
-          , signature="PeptideQuantitativeData"
+          , signature="ggplot2::ggplot"
           , definition=function(theObject, grouping_variable, title = "", font_size = 8) {
-            # First create PCA plot to get the PCA data for density plots
-            pca_plot <- plotPca(theObject, 
-                               grouping_variable = grouping_variable,
-                               shape_variable = NULL,
-                               label_column = "",
-                               title = title,
-                               font_size = font_size)
+            # First try to get data directly from the ggplot object's data element
+            if (!is.null(theObject$data) && is.data.frame(theObject$data)) {
+              pca_data <- as_tibble(theObject$data)
+            } else {
+              # Fall back to other extraction methods
+              pca_data <- as_tibble(ggplot_build(theObject)$data[[1]])
+              
+              # If the data doesn't have PC1/PC2, try to extract from the plot's environment
+              if (!("PC1" %in% colnames(pca_data) && "PC2" %in% colnames(pca_data))) {
+                # Try to get the data from the plot's environment
+                if (exists("data", envir = environment(theObject$mapping$x))) {
+                  pca_data <- as_tibble(get("data", envir = environment(theObject$mapping$x)))
+                } else {
+                  stop("Could not extract PCA data from the ggplot object")
+                }
+              }
+            }
             
-            # Use the existing plotDensity method for ggplot objects
-            density_plot <- plotDensity(pca_plot, 
-                                       grouping_variable = grouping_variable,
-                                       title = title,
-                                       font_size = font_size)
+            # Check if grouping variable exists in the data
+            if (!grouping_variable %in% colnames(pca_data)) {
+              stop(sprintf("grouping_variable '%s' not found in the data", grouping_variable))
+            }
             
-            return(density_plot)
-          })
+            # Create PC1 boxplot
+            pc1_box <- ggplot(pca_data, aes(x = !!sym(grouping_variable), y = PC1, fill = !!sym(grouping_variable))) +
+              geom_boxplot(notch = TRUE) +
+              theme_bw() +
+              labs(title = title,
+                   x = "",
+                   y = "PC1") +
+              theme(
+                legend.position = "none",
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                text = element_text(size = font_size),
+                plot.margin = margin(b = 0, t = 5, l = 5, r = 5),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank()
+              )
+            
+            # Create PC2 boxplot
+            pc2_box <- ggplot(pca_data, aes(x = !!sym(grouping_variable), y = PC2, fill = !!sym(grouping_variable))) +
+              geom_boxplot(notch = TRUE) +
+              theme_bw() +
+              labs(x = "",
+                   y = "PC2") +
+              theme(
+                legend.position = "none",
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                text = element_text(size = font_size),
+                plot.margin = margin(t = 0, b = 5, l = 5, r = 5),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank()
+              )
+            
+            # Combine plots with minimal spacing
+            combined_plot <- pc1_box / pc2_box + 
+              plot_layout(heights = c(1, 1)) +
+              plot_annotation(theme = theme(plot.margin = margin(0, 0, 0, 0)))
+            
+            return(combined_plot)
+          }) 
 
 #'@export
 setMethod(f="plotPearson"
--- R/proteinVsSamplesS4Objects.R
+++ R/proteinVsSamplesS4Objects.R
@@ -1728,16 +1728,75 @@ summariseProteinObject <- function ( theObject) {
 ##----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #'@export
+
 setMethod(f="plotDensity"
-          , signature="gg"
+          , signature="ggplot2::ggplot"
           , definition=function(theObject, grouping_variable, title = "", font_size = 8) {
-            # For gg class objects, create a copy and change its class to ggplot
-            gg_obj <- theObject
-            class(gg_obj) <- "ggplot"
+            # First try to get data directly from the ggplot object's data element
+            if (!is.null(theObject$data) && is.data.frame(theObject$data)) {
+              pca_data <- as_tibble(theObject$data)
+            } else {
+              # Fall back to other extraction methods
+              pca_data <- as_tibble(ggplot_build(theObject)$data[[1]])
+              
+              # If the data doesn't have PC1/PC2, try to extract from the plot's environment
+              if (!("PC1" %in% colnames(pca_data) && "PC2" %in% colnames(pca_data))) {
+                # Try to get the data from the plot's environment
+                if (exists("data", envir = environment(theObject$mapping$x))) {
+                  pca_data <- as_tibble(get("data", envir = environment(theObject$mapping$x)))
+                } else {
+                  stop("Could not extract PCA data from the ggplot object")
+                }
+              }
+            }
             
-            # Then call the ggplot method
-            plotDensity(gg_obj, grouping_variable, title, font_size)
-          })
+            # Check if grouping variable exists in the data
+            if (!grouping_variable %in% colnames(pca_data)) {
+              stop(sprintf("grouping_variable '%s' not found in the data", grouping_variable))
+            }
+            
+            # Create PC1 boxplot
+            pc1_box <- ggplot(pca_data, aes(x = !!sym(grouping_variable), y = PC1, fill = !!sym(grouping_variable))) +
+              geom_boxplot(notch = TRUE) +
+              theme_bw() +
+              labs(title = title,
+                   x = "",
+                   y = "PC1") +
+              theme(
+                legend.position = "none",
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                text = element_text(size = font_size),
+                plot.margin = margin(b = 0, t = 5, l = 5, r = 5),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank()
+              )
+            
+            # Create PC2 boxplot
+            pc2_box <- ggplot(pca_data, aes(x = !!sym(grouping_variable), y = PC2, fill = !!sym(grouping_variable))) +
+              geom_boxplot(notch = TRUE) +
+              theme_bw() +
+              labs(x = "",
+                   y = "PC2") +
+              theme(
+                legend.position = "none",
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                text = element_text(size = font_size),
+                plot.margin = margin(t = 0, b = 5, l = 5, r = 5),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank()
+              )
+            
+            # Combine plots with minimal spacing
+            combined_plot <- pc1_box / pc2_box + 
+              plot_layout(heights = c(1, 1)) +
+              plot_annotation(theme = theme(plot.margin = margin(0, 0, 0, 0)))
+            
+            return(combined_plot)
+          }) 
 
 #'@export
 setMethod(f="plotDensity"
