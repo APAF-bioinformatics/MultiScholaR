f5c5a542eec0a906e0d0004539943ca4289bd4b2
William Klare <william.klare@mq.edu.au>
2025-11-27 17:49:30 +1100

    fixed missing methods

--- R/func_prot_s4_objects.R
+++ R/func_prot_s4_objects.R
@@ -3924,3 +3924,112 @@ setMethod(f = "outputDeResultsAllContrasts",
   message("--- Exiting outputDeResultsAllContrasts ---")
   return(TRUE)
 }) 
+
+# ==========================================
+# Missing S4 Methods recovered from GUI branch
+# ==========================================
+
+##----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#' Get Low Coefficient of Variation Proteins
+#' 
+#' Identifies proteins with the lowest coefficient of variation for use as 
+#' negative controls in RUV normalization.
+#'
+#' @param theObject A ProteinQuantitativeData object
+#' @param percentage_as_neg_ctrl Percentage of proteins to use as negative controls (default: 10)
+#' @param num_neg_ctrl Number of negative control proteins (overrides percentage if set)
+#' @return A logical vector indicating which proteins are selected as negative controls
+#' @export
+setMethod( f = "getLowCoefficientOfVariationProteins"
+           , signature="ProteinQuantitativeData"
+           , definition=function( theObject
+                                  , percentage_as_neg_ctrl = NULL
+                                  , num_neg_ctrl = NULL) {
+
+             percentage_as_neg_ctrl <- checkParamsObjectFunctionSimplify( theObject, "percentage_as_neg_ctrl", 10)
+             num_neg_ctrl <- checkParamsObjectFunctionSimplify( theObject
+                                                                , "num_neg_ctrl"
+                                                                , round(nrow( theObject@protein_quant_table) * percentage_as_neg_ctrl / 100, 0))
+
+             theObject <- updateParamInObject(theObject, "percentage_as_neg_ctrl")
+             theObject <- updateParamInObject(theObject, "num_neg_ctrl")
+
+  list_of_control_genes <- theObject@protein_quant_table |>
+    column_to_rownames(theObject@protein_id_column) |>
+    t() |>
+    as.data.frame() |>
+    summarise( across(everything(), ~sd(.)/mean(.))) |>
+    t() |>
+    as.data.frame() |>
+    dplyr::rename( coefficient_of_variation = "V1") |>
+    tibble::rownames_to_column(theObject@protein_id_column) |>
+    arrange( coefficient_of_variation) |>
+    head(num_neg_ctrl)
+
+  control_gene_index_helper <- theObject@protein_quant_table |>
+    dplyr::select(theObject@protein_id_column) |>
+    mutate( index = row_number()) |>
+    left_join( list_of_control_genes, by = theObject@protein_id_column)  |>
+    mutate( is_selected = case_when( is.na(coefficient_of_variation) ~ FALSE
+                                     , TRUE ~  TRUE ) ) |>
+    arrange( index) |>
+    dplyr::select( !!sym(theObject@protein_id_column), is_selected) |>
+    column_to_rownames(theObject@protein_id_column) |>
+    t()
+
+  control_gene_index <- control_gene_index_helper[1,]
+
+  control_gene_index
+
+})
+
+
+##----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#' Average Technical Replicates
+#' 
+#' Averages protein intensities across technical replicates, collapsing the
+#' design matrix accordingly.
+#'
+#' @param theObject A ProteinQuantitativeData object
+#' @param design_matrix_columns Additional columns to keep in the design matrix
+#' @return The modified ProteinQuantitativeData object with averaged technical replicates
+#' @rdname averageTechReps
+#' @export
+setMethod( f = "averageTechReps"
+           , signature="ProteinQuantitativeData"
+           , definition=function( theObject, design_matrix_columns=c()  ) {
+
+             protein_quant_table <- theObject@protein_quant_table
+             protein_id_column <- theObject@protein_id_column
+             design_matrix <- theObject@design_matrix
+             group_id <- theObject@group_id
+             sample_id <- theObject@sample_id
+             replicate_group_column <- theObject@technical_replicate_id
+
+             theObject@protein_quant_table <- protein_quant_table |>
+               pivot_longer( cols = !matches( protein_id_column)
+                             , names_to = sample_id
+                             , values_to = "Log2.Protein.Imputed") |>
+               left_join( design_matrix
+                          , by = join_by( !!sym(sample_id) == !!sym(sample_id))) |>
+               group_by( !!sym(protein_id_column), !!sym(replicate_group_column) )  |>
+               summarise( Log2.Protein.Imputed = mean( Log2.Protein.Imputed, na.rm = TRUE)) |>
+               ungroup() |>
+               pivot_wider( names_from = !!sym(replicate_group_column)
+                            , values_from = Log2.Protein.Imputed)
+
+              theObject@sample_id <- theObject@technical_replicate_id
+
+              theObject@design_matrix <- design_matrix |>
+                dplyr::select(-!!sym( sample_id)) |>
+                dplyr::select(all_of( unique( c( replicate_group_column,  group_id,  design_matrix_columns) ))) |>
+                distinct()
+
+              theObject@sample_id <- replicate_group_column
+              theObject@technical_replicate_id <- NA_character_
+
+              theObject <- cleanDesignMatrix(theObject)
+
+              theObject
+
+           }) 
